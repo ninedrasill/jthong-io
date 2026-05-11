0cb250604baea9658459d049fa62d80139317fdc
JT Hong <ninedrasill@gmail.com>
2026-05-12 00:40:35 +0900

    tweak: overlay 80% → 78%

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
           }}
         />
         <div
-          className="fixed left-0 top-0 w-screen -z-10 bg-black/80"
+          className="fixed left-0 top-0 w-screen -z-10 bg-black/[0.78]"
           style={{ height: "100lvh" }}
         />
         {children}
